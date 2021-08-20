c980cff2ad67ca6e9d833be764bdeba2476874b4
Usman Siddique <usmansiddique1515@gmail.com>
2021-08-21 03:11:13 +0500

    added enable, quadrant value output

--- src/divider.v
+++ src/divider.v
@@ -10,17 +10,38 @@ module divider (
     output reg [`DATA_WIDTH-1 : 0]  data_out
 );
 
+wire [`DATA_WIDTH-1:0] divisor = `DATA_WIDTH'd360;
+
 always @(posedge clk ) begin
     if (reset_n) begin
         if (en_divider) begin
-            
+            data_out <= data_in % divisor;
+			// --------------------------------------- 
+			if(data_in >= 'd0 && data_in <= 'd90) begin
+                quadrant <= 2'd0;
+			end
+			else if(data_in >= 'd91 && data_in <= 'd180) begin
+                quadrant <= 2'd1;
+				// $display("Input angle [%3d] | Quadrant [%2d] | Data output [%3d]",data_in,quadrant,data_out);
+			end
+			else if(data_in >= 'd181 && data_in <= 'd270) begin
+                quadrant <= 2'd2;
+				// $display("Input angle [%3d] | Quadrant [%2d] | Data output [%3d]",data_in,quadrant,data_out);
+			end
+			else if(data_in >= 'd271 && data_in <= 'd360) begin
+                quadrant <= 2'd3;
+				//$display("Input angle [%3d] | Quadrant [%2d] | Data output [%3d]",data_in,quadrant,data_out);
+			end
+            $display("Input angle [%3d] | Quadrant [%2d] | Data output [%3d]",data_in,quadrant,data_out);
         end
         else begin
-            
+            data_out <= 'dz;
+            quadrant <= 'dz;
         end
     end
     else begin
-        
+        data_out <= 'dz;
+        quadrant <= 'dz;
     end
 end
 
