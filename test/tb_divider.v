b7b46d6c5b5c6a052a1473251881d3ec1ff317a6
Usman Siddique <usmansiddique1515@gmail.com>
2021-08-21 04:04:13 +0500

    added main loop and vcd dump loop

--- test/tb_divider.v
+++ test/tb_divider.v
@@ -10,8 +10,22 @@ integer max1 = `DATA_WIDTH'd720;
 integer num1 = 0;
 
 // -------------------------------------------------------- IO
+reg     clk;
+reg     reset_n;
+reg     en_divider;
+reg     [`DATA_WIDTH-1:0]   data_in;
+wire    [1:0]               quadrant;
+wire    [`DATA_WIDTH-1:0]   data_out;
 
 // -------------------------------------------------------- module instantiation
+divider DUT_divider(
+    .clk        (clk        ),
+    .reset_n    (reset_n    ),
+    .en_divider (en_divider ),
+    .data_in    (data_in    ),
+    .quadrant   (quadrant   ),
+    .data_out   (data_out   )
+);
 
 // -------------------------------------------------------- clk generator
 always begin
@@ -20,8 +34,49 @@ always begin
 end
 
 // -------------------------------------------------------- main loop
+initial begin
+    // --------------------------------------- reset module
+    $display("-----------------------------------------------------------------\n");
+    repeat(2) @(posedge clk) begin
+        reset_n = 1'b0;
+        reset_n = 1'b1;
+    end
+    
+    // --------------------------------------- send 20 random -ve inputs
+    $display("-----------------------------------------------------------------\n");
+    repeat(20) begin
+        @(posedge clk) begin
+            reset_n = 1'b1;
+            en_divider = 1'b1;
+            data_in = min1 + {$random} % (max1-min1);
+        end
+        #T $display("Input angle [%3d] | Quadrant [%2d] | Data output [%3d]",data_in,quadrant,data_out);
+    end
+    
+    // --------------------------------------- check for all input values
+    $display("-----------------------------------------------------------------\n");
+    repeat(145) begin
+        @(posedge clk) begin
+            reset_n = 1'b1;
+            en_divider = 1'b1;
+            data_in = num1;
+        end
+        #T $display("Input angle [%3d] | Quadrant [%2d] | Data output [%3d]",data_in,quadrant,data_out);
+        num1 = num1 + 5;
+    end
+    @(posedge clk)  reset_n = 1'b0;
+    @(posedge clk)  reset_n = 1'b1;
+    
+    $display("-----------------------------------------------------------------\n");
+    $stop;
+end
+
+// -------------------------------------------------------- dump vcd
+initial begin
+    $dumpfile("bin/tb_divider.vcd");
+    $dumpvars(0,DUT_divider);
+end
 
 // -------------------------------------------------------- end of module
-
 
 endmodule
