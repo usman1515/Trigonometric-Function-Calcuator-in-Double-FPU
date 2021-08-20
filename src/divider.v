9ed7d1635c017509e94f7d464ce5577cc4f2f467
Usman Siddique <usmansiddique1515@gmail.com>
2021-08-21 02:28:09 +0500

    added module def in divider

--- src/divider.v
+++ src/divider.v
@@ -1,1 +1,27 @@
+`include "src/defines.v"
+`timescale 1ns/100ps
 
+module divider (
+    input wire                      clk,
+    input wire                      reset_n,
+    input wire                      en_divider,
+    input wire [`DATA_WIDTH-1 : 0]  data_in,
+    output reg [1:0]                quadrant,
+    output reg [`DATA_WIDTH-1 : 0]  data_out
+);
+
+always @(posedge clk ) begin
+    if (reset_n) begin
+        if (en_divider) begin
+            
+        end
+        else begin
+            
+        end
+    end
+    else begin
+        
+    end
+end
+
+endmodule
