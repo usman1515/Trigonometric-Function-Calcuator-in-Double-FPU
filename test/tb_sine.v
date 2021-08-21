abb0c3f7276f213de91643f0ecc959bc7ef414ab
Usman Siddique <usmansiddique1515@gmail.com>
2021-08-21 17:15:50 +0500

    added clk block, dumpvars in tb sineLUT

--- test/tb_sine.v
+++ test/tb_sine.v
@@ -1,1 +1,32 @@
+`include "src/defines.v"
+`timescale 1ns/100ps
 
+module tb_sine;
+
+// -------------------------------------------------------- parameters
+localparam T = 2;
+integer min1 = `DATA_WIDTH'd0;
+integer max1 = `DATA_WIDTH'd720;
+integer num1 = 0;
+
+// -------------------------------------------------------- IO
+
+// -------------------------------------------------------- module instantiation
+
+// -------------------------------------------------------- clk generator
+always begin
+    #(T/2) clk = 1'b0;
+    #(T/2) clk = 1'b1;
+end
+
+// -------------------------------------------------------- main loop
+
+// -------------------------------------------------------- dump vcd
+initial begin
+    $dumpfile("bin/tb_sine.vcd");
+    $dumpvars(0,DUT_sineLUT);
+end
+
+// -------------------------------------------------------- end of module
+
+endmodule
