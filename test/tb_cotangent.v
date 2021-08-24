846f71718938225b94b3aed38dacdce01369f6c1
Usman Siddique <usmansiddique1515@gmail.com>
2021-08-25 01:03:46 +0500

    added module instance, main loop for tb cot

--- test/tb_cotangent.v
+++ test/tb_cotangent.v
@@ -18,6 +18,14 @@ reg     [`DATA_WIDTH-1 : 0]      data_in;
 wire    [(`DATA_WIDTH*2)-1 : 0]  data_out;
 
 // -------------------------------------------------------- module instantiation
+cotangent_LUT DUT_cotangentLUT(
+    .clk            (clk            ),
+    .reset_n        (reset_n        ),
+    .en_cotangent   (en_cotangent   ),
+    .quadrant       (quadrant       ),
+    .data_in        (data_in        ),
+	.data_out       (data_out       )
+);
 
 // -------------------------------------------------------- clk generator
 always begin
@@ -26,6 +34,44 @@ always begin
 end
 
 // -------------------------------------------------------- main loop
+initial begin
+    // --------------------------------------- reset module
+    $display("-----------------------------------------------------------------\n");
+    repeat(2) @(posedge clk) begin
+        reset_n = 1'b0;
+        reset_n = 1'b1;
+    end
+    
+    // --------------------------------------- check for all input values
+    $display("-----------------------------------------------------------------\n");
+    repeat(91) begin
+        @(posedge clk) begin
+            reset_n = 1'b1;
+            en_cotangent = 1'b1;
+            quadrant = 1'b0;
+            data_in = num1;
+        end
+        #T $monitor("Input angle [%3d] | Sign [%1b] | Cotangent DFPU value = %16h",data_in,data_out[63],data_out);
+        num1 = num1 + 1;
+    end
+    @(posedge clk)  reset_n = 1'b0;
+    @(posedge clk)  reset_n = 1'b1;
+    
+    // --------------------------------------- send 20 random -ve inputs
+    $display("-----------------------------------------------------------------\n");
+    repeat(20) begin
+        @(posedge clk) begin
+            reset_n = 1'b1;
+            en_cotangent = 1'b1;
+            quadrant = 1'b1; 
+            data_in = min1 + {$random} % (max1-min1);
+        end
+        #T $display("Input angle [%3d] | Sign [%1b] | Cotangent DFPU value = %16h",data_in,data_out[63],data_out);
+    end
+    
+    $display("-----------------------------------------------------------------\n");
+    $finish;
+end
 
 // -------------------------------------------------------- dump vcd
 initial begin
