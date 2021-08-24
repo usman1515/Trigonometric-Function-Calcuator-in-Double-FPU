d8e4ff61a733f657c09db417f0df11953c3fd35c
Usman Siddique <usmansiddique1515@gmail.com>
2021-08-25 00:53:05 +0500

    added module instance, main loop for tb cosec

--- test/tb_cosecant.v
+++ test/tb_cosecant.v
@@ -18,6 +18,14 @@ reg     [`DATA_WIDTH-1 : 0]      data_in;
 wire    [(`DATA_WIDTH*2)-1 : 0]  data_out;
 
 // -------------------------------------------------------- module instantiation
+cosecant_LUT DUT_cosecantLUT(
+    .clk         (clk           ),
+    .reset_n     (reset_n       ),
+    .en_cosecant (en_cosecant   ),
+    .quadrant    (quadrant      ),
+    .data_in     (data_in       ),
+	.data_out    (data_out      )
+);
 
 // -------------------------------------------------------- clk generator
 always begin
@@ -26,6 +34,44 @@ always begin
 end
 
 // -------------------------------------------------------- main loop
+initial begin
+    // --------------------------------------- reset module
+    $display("-----------------------------------------------------------------\n");
+    repeat(2) @(posedge clk) begin
+        reset_n = 1'b0;
+        reset_n = 1'b1;
+    end
+    
+    // --------------------------------------- check for all input values
+    $display("-----------------------------------------------------------------\n");
+    repeat(91) begin
+        @(posedge clk) begin
+            reset_n = 1'b1;
+            en_cosecant = 1'b1;
+            quadrant = 1'b0;
+            data_in = num1;
+        end
+        #T $monitor("Input angle [%3d] | Sign [%1b] | Cosecant DFPU value = %16h",data_in,data_out[63],data_out);
+        num1 = num1 + 1;
+    end
+    @(posedge clk)  reset_n = 1'b0;
+    @(posedge clk)  reset_n = 1'b1;
+    
+    // --------------------------------------- send 20 random -ve inputs
+    $display("-----------------------------------------------------------------\n");
+    repeat(20) begin
+        @(posedge clk) begin
+            reset_n = 1'b1;
+            en_cosecant = 1'b1;
+            quadrant = 1'b1; 
+            data_in = min1 + {$random} % (max1-min1);
+        end
+        #T $display("Input angle [%3d] | Sign [%1b] | Cosecant DFPU value = %16h",data_in,data_out[63],data_out);
+    end
+    
+    $display("-----------------------------------------------------------------\n");
+    $finish;
+end
 
 // -------------------------------------------------------- dump vcd
 initial begin
