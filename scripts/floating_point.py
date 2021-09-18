1ad5fa25acea9905ba11946a5927b370ed3313f9
Usman Siddique <usmansiddique1515@gmail.com>
2021-09-18 17:51:28 +0500

    created class IEEE754DFPU

---
+++ scripts/floating_point.py
@@ -0,0 +1,40 @@
+'''
+References:
+
+https://www.wikihow.com/Convert-a-Number-from-Decimal-to-IEEE-754-Floating-Point-Representation
+https://babbage.cs.qc.cuny.edu/IEEE-754.old/Decimal.html
+'''
+
+import argparse
+import logging
+import math
+import os
+os.system('clear')
+
+# =============================================================================
+# ================================ Begin Class ================================
+# =============================================================================
+
+# class IEEE 754 Double Floating Point Unit
+class IEEE754DFPU():
+    # ----------------------------------------------------------------------------- Variables
+    
+    
+    # ----------------------------------------------------------------------------- Functions
+    def __init__(self):
+        pass
+    
+    def getPrecison(self):
+        pass
+
+
+
+# =============================================================================
+# ================================= End Class =================================
+# =============================================================================
+
+def main():
+    print('usman')
+
+if __name__ == '__main__':
+    main()
