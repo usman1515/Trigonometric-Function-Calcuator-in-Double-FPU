abe1d4f94e1f2f52f0d6a6331f85b75c6679ead0
Usman Siddique <usmansiddique1515@gmail.com>
2021-09-18 20:18:41 +0500

    created function convertInputNum2Bin

--- scripts/floating_point.py
+++ scripts/floating_point.py
@@ -20,12 +20,33 @@ class IEEE754DFPU():
     
     # ----------------------------------------------------------------------------- Variables
     __myargs = argparse.Namespace
+    __inputNum = float
+    __wholeNumBin = str
+    __decNumBin = str
     
+    
+    spSign = str
+    dpSign = str
+    spExponent = str
+    dpExponent = str
+    spMantissa = str
+    dpMantissa = str
     
     # ----------------------------------------------------------------------------- Functions
     # constructor
     def __init__(self):
         self.__myargs = argparse.Namespace
+        self.__inputNum = 0.0
+        self.__wholeNumBin = ''
+        self.__decNumBin = '0b'
+        
+        
+        self.spSign = str
+        self.dpSign = str
+        self.spExponent = str
+        self.dpExponent = str
+        self.spMantissa = str
+        self.dpMantissa = str
     
     
     # setup arguments for class
@@ -57,9 +78,34 @@ class IEEE754DFPU():
         else:
             self.__myargs.doubleprecison = 1
             self.__myargs.singleprecison = 0
+        # print verbosity
         if self.__myargs.verbose:
             print('Single Precison: ',self.__myargs.singleprecison)
             print('Double Precison: ',self.__myargs.doubleprecison)
+    
+    
+    # convert whole number to binary
+    def convertInputNum2Bin(self,number):
+        # get num as arg or input
+        if self.__myargs.inputvalue:
+            self.__inputNum = self.__myargs.inputvalue
+        else:
+            self.__inputNum = number
+        # split input num
+        decNum, wholeNum = math.modf(self.__inputNum)
+        # convert whole num to binary
+        self.__wholeNumBin = bin(int(wholeNum))
+        # convert dec num to binary
+        tempdecNum = decNum
+        while not tempdecNum >= 1:
+            tempdecNum *= 2
+            # print('Each dec num stage: ',tempdecNum,int(tempdecNum))
+            self.__decNumBin += str(int(tempdecNum))
+        # print verbosity
+        if self.__myargs.verbose:
+            print('Input Number: ',self.__inputNum)
+            print('{:^5s}{:>10d} (10) ---> {:<15s} (2)'.format(' ',int(wholeNum),self.__wholeNumBin))
+            print('{:^5s}{:>10f} (10) ---> {:<15s} (2)'.format(' ',decNum,self.__decNumBin))
 
 # =============================================================================
 # ================================= End Class =================================
@@ -72,6 +118,8 @@ def main():
     obj1.setArguments()
     # set precison level
     obj1.getPrecison()
+    # convert dec num to bin num
+    obj1.convertInputNum2Bin(number=85.125)
 
 
 if __name__ == '__main__':
