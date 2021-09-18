f5f07bfd5ad9eadba6b32141d44ec354e53db01b
Usman Siddique <usmansiddique1515@gmail.com>
2021-09-18 18:38:22 +0500

    created function setArguments

--- scripts/floating_point.py
+++ scripts/floating_point.py
@@ -17,13 +17,38 @@ os.system('clear')
 
 # class IEEE 754 Double Floating Point Unit
 class IEEE754DFPU():
+    
     # ----------------------------------------------------------------------------- Variables
+    __myargs = argparse.Namespace
     
     
     # ----------------------------------------------------------------------------- Functions
+    # constructor
     def __init__(self):
-        pass
+        self.__myargs = argparse.Namespace
     
+    
+    # setup arguments for class
+    def setArguments(self):
+        # set arguments for class IEEE754DFPU
+        parser = argparse.ArgumentParser(prog='IEEE-754 Double Floating Point Unit',
+                                        usage='%(prog)s [options] path',
+                                        description='IEEE-754 single and double precision hexadecimal floating point unit generator',
+                                        epilog='Happy trigonometry :)')
+        # list of all possible args for IEEE754DFPU
+        parser.add_argument('-dp','--doubleprecison',
+                            type=int,default=1,metavar='',required=False,nargs='?',help='Set output value to have double precision')
+        parser.add_argument('-sp','--singleprecison',
+                            type=int,default=0,metavar='',required=False,nargs='?',help='Set output value to have single precision')        
+        parser.add_argument('-num','--inputvalue',
+                        type=float,default=0,metavar='',required=False,nargs='?',help='Input user defined decimal value')
+        group = parser.add_mutually_exclusive_group()
+        parser.add_argument('-v','--verbose',
+                            type=int,default=0,metavar='',required=False,nargs='?',help='print verbose')
+        self.__myargs = parser.parse_args()
+    
+    
+    # get precison level ie sp/dp
     def getPrecison(self):
         pass
 
@@ -34,7 +59,12 @@ class IEEE754DFPU():
 # =============================================================================
 
 def main():
-    print('usman')
+    # 
+    obj1 = IEEE754DFPU()
+    # 
+    obj1.setArguments()
+    obj1.getPrecison()
+    # print(type(obj1.__myargs))
 
 if __name__ == '__main__':
     main()
