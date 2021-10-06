99b52d1525fa8977112f22eba7dad2dd38b08a15
Usman Siddique <usmansiddique1515@gmail.com>
2021-10-07 00:06:16 +0500

    created function setArguments

--- scripts/double_precision.py
+++ scripts/double_precision.py
@@ -20,6 +20,7 @@ os.system('clear')
 
 class DoublePrecision():
     # ------------------------------------------- Variables
+    __myargs = argparse.Namespace
     __inputNum = float
     __spSign = str
     __dpSign = str
@@ -27,6 +28,7 @@ class DoublePrecision():
     # ------------------------------------------- Functions
     # constructor
     def __init__(self):
+        self.__myargs = argparse.Namespace
         self.__inputNum = 0.0
         self.__spSign = ''
         self.__dpSign = ''
@@ -34,7 +36,21 @@ class DoublePrecision():
     
     # setup arguments for class
     def setArguments(self):
-        pass
+        # ---------- set arguments for class IEEE754DFPU
+        parser = argparse.ArgumentParser(prog='IEEE-754 Double Floating Point Unit',
+                                        usage='%(prog)s [options] path',
+                                        description='IEEE-754 double precision hexadecimal floating point unit generator',
+                                        epilog='Happy trigonometry :)')
+        # ---------- list of all possible args for Double Precision
+        parser.add_argument('-num','--inputvalue',
+                        type=float,default=0,metavar='',required=False,nargs='?',help='Input user defined decimal value')
+        parser.add_argument('-sign','--signbit',
+                            type=int,default=0,metavar='',required=False,help='Include sign bit in hex num')
+        parser.add_argument('-v','--verbose',
+                            type=int,default=0,metavar='',required=False,nargs='?',help='print verbose')
+        parser.add_argument('-d','--debug',
+                            type=int,default=0,metavar='',required=False,nargs='?',help='print debugging')
+        self.__myargs = parser.parse_args()
     
     
     # Determine sign of the number and display in binary format
