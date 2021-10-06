6492f712e446e9e1f6c0eaf345c1ee7a9faa0158
Usman Siddique <usmansiddique1515@gmail.com>
2021-10-07 00:04:58 +0500

    created function getSign

--- scripts/double_precision.py
+++ scripts/double_precision.py
@@ -7,7 +7,7 @@ https://class.ece.iastate.edu/arun/Cpre305/ieee754/ie4.html
 https://class.ece.iastate.edu/arun/CprE281_F05/ieee754/ie5.html
 '''
 
-from mpmath import *
+from mpmath import mp
 import argparse
 #import math
 import re
@@ -19,19 +19,50 @@ os.system('clear')
 # =============================================================================
 
 class DoublePrecision():
-    # ----------------------------------------------------------------------------- Variables
-    # ----------------------------------------------------------------------------- Functions
+    # ------------------------------------------- Variables
+    __inputNum = float
+    __spSign = str
+    __dpSign = str
+    
+    # ------------------------------------------- Functions
     # constructor
-    pass
+    def __init__(self):
+        self.__inputNum = 0.0
+        self.__spSign = ''
+        self.__dpSign = ''
+    
+    
+    # setup arguments for class
+    def setArguments(self):
+        pass
+    
+    
+    # Determine sign of the number and display in binary format
+    def getSign(self):
+        if self.__inputNum >= 0:    
+            self.__spSign = '0'
+            self.__dpSign = '0'
+            ## print verbosity
+            #if self.__myargs.verbose:
+            #    print('{:^5s}{:<21s}{:<5s}'.format(' ','Sign bit = ',"1'b0"))
+        else:
+            self.__spSign = '1'
+            self.__dpSign = '1'
+            ## print verbosity
+            #if self.__myargs.verbose:
+            #    print('{:^5s}{:<21s}{:<5s}'.format(' ','Sign bit = ',"1'b1"))
+
 
 # =============================================================================
 # ================================= End Class =================================
 # =============================================================================
 
 def main():
-    for i in range(0,91,1):
-        # val = Decimal(math.sin(math.radians(i)))
-        print('Input({:^2d}) ----> {:<60f} {:>3d}'.format(i,val,len(str(val))))
+    obj1 = DoublePrecision()
+    obj1.getSign()
+    #for i in range(0,91,1):
+    #    # val = Decimal(math.sin(math.radians(i)))
+    #    print('Input({:^2d}) ----> {:<60f} {:>3d}'.format(i,val,len(str(val))))
 
 if __name__ == '__main__':
     main()
