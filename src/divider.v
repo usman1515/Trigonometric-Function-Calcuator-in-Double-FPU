26c19ce2e724615a85c93861e78412785d486a0d
Usman Siddique <usmansiddique1515@gmail.com>
2021-10-20 00:55:14 +0500

    added mod functionality in src divider

--- src/divider.v
+++ src/divider.v
@@ -6,34 +6,18 @@ module divider (
     input wire                      reset_n,
     input wire                      en_divider,
     input wire [`DATA_WIDTH-1 : 0]  data_in,
-    output reg [1:0]                quadrant,
+    //output reg [1:0]                quadrant,
     output reg [`DATA_WIDTH-1 : 0]  data_out
 );
 
 wire [`DATA_WIDTH-1:0] divisor = `DATA_WIDTH'd360;
+wire [`DATA_WIDTH-1:0] divider_out; //quotient
 
 always @(posedge clk ) begin
     if (reset_n) begin
         if (en_divider) begin
             data_out <= data_in % divisor;
-			// --------------------------------------- 
-			if(data_out >= 'd0 && data_out <= 'd90 || data_out == 'd360)
-                quadrant <= 2'd0;
-			else if(data_out >= 'd91 && data_out <= 'd180)
-                quadrant <= 2'd1;
-			else if(data_out >= 'd181 && data_out <= 'd270)
-                quadrant <= 2'd2;
-			else if(data_out >= 'd271 && data_out < 'd360)
-                quadrant <= 2'd3;
         end
-        else begin
-            data_out <= 'dz;
-            quadrant <= 'dz;
-        end
-    end
-    else begin
-        data_out <= 'dz;
-        quadrant <= 'dz;
     end
 end
 
